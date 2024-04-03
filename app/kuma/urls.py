4e4ad62334b151aada67714837e778493c8d0689
Marcelo200221 <ma.darras@duocuc.cl>
2024-04-03 18:52:28 -0300

    Se arregla el codigo para la bodega y se borran los templates innecesarios

--- app/kuma/urls.py
+++ app/kuma/urls.py
@@ -1,21 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.cargarInicio),
     path('inicio', views.cargarInicio),
     path('login', views.cargarSesion),
-    path('bandana', views.cargarBandana),
-    path('correa', views.cargarCorreas),
-    path('colgante', views.cargarColgantes),
-    path('juguete', views.cargarJuguetes),
-    path('agregarProducto', views.cargarAgregarProducto),
-    path('agregarProductoForm', views.agregarProducto),
-    path('editarProducto/<sku>', views.cargarEditarProductos),
-    path('editarProductoForm', views.editarProductoForm),
-    path('eliminarProducto/<sku>', views.eliminarProducto),
-    path('registrar', views.cargarAgregarUsuario),
-    path('agregarUsuarioForm', views.agregarUsuario),
-    path('comida', views.cargarComida),
-    path('actualizar_stock/', views.actualizar_stock, name='actualizar_stock')
+    path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]

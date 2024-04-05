679e04ebfdd6776bdafca0079cc64ebfa18440b2
unknown <Cetecom@SJAPCA03LC0324.duoc.acad>
2024-04-05 12:25:42 -0300

    Se agregan metodos, agregar, eliminar, editar y crear usuario

--- app/kuma/urls.py
+++ app/kuma/urls.py
@@ -6,5 +6,11 @@ urlpatterns = [
     path('', views.cargarInicio),
     path('inicio', views.cargarInicio),
     path('login', views.cargarSesion),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('agregarProductoForm', views.agregarProducto),
+    path('eliminarProducto/<sku>', views.eliminarProducto),
+    path('editarProducto/<sku>', views.cargarEditarProductos),
+    path('editarProductoForm', views.editarProductoForm),
+    path('registrar', views.cargarAgregarUsuario),
+    path('agregarUsuarioForm', views.agregarUsuario),
 ]
